26211298fbfe9cf66c3615a068cd19203300395d
fumikito <guy@hametuha.com>
2023-04-16 23:46:08 +0900

    Fix test results.

--- tests/test-string-utility.php
+++ tests/test-string-utility.php
@@ -11,6 +11,6 @@ class StringUtilityTest extends WP_UnitTestCase {
 	public function test_column() {
 		$parser = new \Sfwj\SfwjWeb\Tools\CsvParser\NormalMemberCsvParser(0);
 		$this->assertEquals( 3, $parser->column_to_index( 'D' ) );
-		$this->assertEquals( 29, $parser->column_to_index( 'AC' ) );
+		$this->assertEquals( 28, $parser->column_to_index( 'AC' ) );
 	}
 }
