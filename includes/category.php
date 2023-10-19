bdfe0cc5b5da180498061423b250cfe8249c573d
Masahiko Kawai <kawai@cherrypieweb.com>
2023-10-19 20:51:26 +0900

    fix lint

--- includes/category.php
+++ includes/category.php
@@ -45,10 +45,10 @@ add_action( 'lightning_loop_after',
 
 /**
  * VK_投稿リストブロックをニュース一覧として使用する際、特定のカテゴリーを除外する
- * 
+ *
  * - ブロックのclassに "news-list" が含まれていたらニュース一覧と判断する
  * - 除外するカテゴリーは、function内の $exclude_cat_slug にスラッグを直書きする
- * 
+ *
  * @param array(str) $exclude_cat_slug カテゴリースラッグ
  * @return \WP_Query\args
  *
@@ -58,15 +58,15 @@ add_filter(
 	function( $args, $attributes ) {
 		// 除外するカテゴリーのスラッグを指定
 		$exclude_cat_slug = array( 'exclude-list', 'events' );
-		if ( !is_array( $exclude_cat_slug ) ) {
+		if ( ! is_array( $exclude_cat_slug ) ) {
 			return;
 		}
-		if ( strpos( $attributes[ 'className' ], 'news-list' ) !== false ) {
+		if ( strpos( $attributes['className'], 'news-list' ) !== false ) {
 			$exclude_cat = array();
 			foreach ( $exclude_cat_slug as $slug ) {
-				$exclude_cat[] = get_category_by_slug( $slug ) -> cat_ID;
+				$exclude_cat[] = get_category_by_slug( $slug )->cat_ID;
 			}
-			
+
 			$args['tax_query'][] = array(
 				'taxonomy' => 'category',
 				'terms'    => $exclude_cat,
